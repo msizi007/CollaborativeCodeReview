136376bbf1347573e90e220120645d8c9f99aaab
Msizi <msizimwelase007@gmail.com>
2025-11-06 15:13:03 +0200

    added 404 middleware and used json

--- server.ts
+++ server.ts
@@ -1,11 +1,20 @@
 // IMPORTS
 require("dotenv").config();
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 
 const app: Express = express();
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello! Welcome to the Collaboration Code Plartform...");
+});
+
+// 404 ERROR - Middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    sucess: false,
+    message: "Not found! The route you have selected does not exist",
+  });
 });
 
 app.listen(process.env.PORT, () => {
